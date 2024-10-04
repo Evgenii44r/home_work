a0969ba513736c18209eb967eedda84c611d013c
Evgenii44r <evgenii44r@gmail.com>
2024-10-04 12:30:49 +0300

    add file

--- cycles.py
+++ cycles.py
@@ -1,2 +1,21 @@
-for i in range(10):
-    print('hello')
+#for i in range(10):
+    #print('hello')
+# collection = [1,2,3,4,5,6,7,8,9,10]
+# n = 10
+# for i in collection:
+      # i = n*i
+      # print(i)
+# for i in range(10, 5, -2):
+#       print(i)
+# n = 6
+# while n > 0:
+#         n -= 1
+#         print(n)
+# n = 1
+# while n < 6:
+#     print('Цикл выполнился', n, 'раз(а)')
+#     n = n+1
+# n = 6
+# while n > 0:
+#         n -= 1
+#         print(n)
