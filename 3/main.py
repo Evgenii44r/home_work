991ab804d77f5ad0f4ab06bd492240cfca6a9643
Evgenii44r <evgenii44r@gmail.com>
2024-11-07 20:35:43 +0300

    add

--- 3/main.py
+++ 3/main.py
@@ -1,35 +1,46 @@
 from tank import Tank
-from tkinter import*
-
+from tkinter import *
+import world
 KEY_W = 87
 KEY_S = 83
 KEY_A = 65
 KEY_D = 68
 
+FPS = 60
+
+def update():
+    player.update()
+    enemy.update()
+    check_collision()
+    w.after(1000 // FPS, update)
+
 def check_collision():
-    if player.intersects(enemy):
-        print('танки столькнулись')
+    player.intersects(enemy)
+    enemy.intersects(player)
 
 def key_press(event):
-    if event.keycode ==KEY_W:
+    if event.keycode == KEY_W:
         player.forvard()
-    if event.keycode ==KEY_S:
-        player.dackward()
-    if event.keycode ==KEY_A:
+    if event.keycode == KEY_S:
+        player.backward()
+    if event.keycode == KEY_A:
         player.left()
-    if event.keycode ==KEY_D:
+    if event.keycode == KEY_D:
         player.right()
     check_collision()
 
 w = Tk()
-w.title('танки на минималках 2.0')
+w.title('')
+canv = Canvas(w, width=world.WIDTH, height=world.HEIGHT, bg='alice blue')
 canv = Canvas(w, width=800, height=600, bg='alice blue')
 canv.pack()
 
-player = Tank(canvas=canv,x=100, y=50, ammo=100)
+player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1,bot = False)
 
-enemy = Tank(canvas=canv, x=300, y=300, ammo=100)
+enemy = Tank(canvas=canv, x=300, y=300, ammo=100, speed=1,bot = True)
+
+enemy.set_target(player)
 
 w.bind('<KeyPress>', key_press)
-
+update()
 w.mainloop()
