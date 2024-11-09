439a25fab4c16f8b0b32f152a70ce76674d464ff
Evgenii44r <evgenii44r@gmail.com>
2024-11-09 15:41:40 +0300

    add

--- 3/main.py
+++ 3/main.py
@@ -2,6 +2,7 @@
 from tank import Tank
 from tkinter import *
 import world
+KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN = 37, 39, 38, 40
 KEY_W = 87
 KEY_S = 83
 KEY_A = 65
@@ -9,10 +10,13 @@ KEY_D = 68
 
 FPS = 60
 def update():
+    world.set_camera_xy(player.get_x()-world.SCREEN_WIDTH//2 + player.get_size()//2, player.get_y()-world.SCREEN_HEIGHT//2 + player.get_size()//2)
     player.update()
     enemy.update()
+    neutral.update()
     check_collision()
     w.after(1000 // FPS, update)
+
 
 def check_collision():
     player.intersects(enemy)
@@ -27,19 +31,29 @@ def key_press(event):
         player.left()
     if event.keycode == KEY_D:
         player.right()
+    elif event.keycode == KEY_UP:
+        world.move_camera(0, -5)
+    elif event.keycode == KEY_DOWN:
+        world.move_camera(0, 5)
+    elif event.keycode == KEY_LEFT:
+        world.move_camera(-5, 0)
+    elif event.keycode == KEY_RIGHT:
+        world.move_camera(5, 0)
     check_collision()
 
 w = Tk()
 w.title('')
-canv = Canvas(w, width=world.WIDTH, height=world.HEIGHT, bg='alice blue')
-canv = Canvas(w, width=800, height=600, bg='alice blue')
+canv = Canvas(w, width=world.SCREEN_WIDTH, height=world.SCREEN_HEIGHT, bg='alice blue')
+
 canv.pack()
 
 player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1,bot = False)
 
 enemy = Tank(canvas=canv, x=300, y=300, ammo=100, speed=1,bot = True)
+neutral = Tank(canvas=canv, x=200, y=200, ammo=100, speed=1, bot = False)
 
 enemy.set_target(player)
+neutral.stop()
 
 w.bind('<KeyPress>', key_press)
 update()
